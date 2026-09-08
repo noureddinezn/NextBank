53a4fda5c409703768fc6ec539271ca1f5106c48
noureddinezn <noureddinezouana@gmail.com>
2026-09-08 15:57:18 +0100

    NEX-13: Création de la classe abstraite Personne avec encapsulation

--- SRC/model/Personne.java
+++ SRC/model/Personne.java
@@ -1,5 +1,47 @@
 package SRC.model;
 
-public class Personne {
-    
+public abstract class Personne {
+protected String nom;
+    protected String prenom;
+    protected String email;
+    protected String motDePasse;
+       public Personne(String nom, String prenom, String email, String motDePasse) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.motDePasse = motDePasse;
+    }
+    public String getNom() {
+        return nom;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMotDePasse() {
+        return motDePasse;
+    }
+
+    public void setMotDePasse(String motDePasse) {
+        this.motDePasse = motDePasse;
+    }
+
+    public boolean verifierMotDePasse(String motDePasseSaisi) {
+        return this.motDePasse.equals(motDePasseSaisi);
+    }
+
 }
