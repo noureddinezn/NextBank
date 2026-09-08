3cffff5c9a5ed0755cea09d9d12e5a071bf0a8ac
noureddinezn <noureddinezouana@gmail.com>
2026-09-08 16:21:18 +0100

    NEX-14: Implémentation initiale de Client

--- SRC/model/Client.java
+++ SRC/model/Client.java
@@ -1,5 +1,21 @@
 package SRC.model;
+import java.util.HashMap;
+import java.util.Map;
 
-public class Client {
+public class Client  extends Personne{
+   private String idClient;
+
+    // private Map<String, Compte> comptes;
+
+  public Client(String nom, String prenom, String email, String motDePasse, String idClient) {
+        super(nom, prenom, email, motDePasse);
+        this.idClient = idClient; 
+        // this.comptes = new HashMap<>();
+    }
+
+    public String getIdClient() {
+        return idClient;
+    }
+
     
 }
