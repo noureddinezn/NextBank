eed9f63e7d54b929f595d925f464ff2bb11c73a9
noureddinezn <noureddinezouana@gmail.com>
2026-09-08 16:52:45 +0100

    NEX-16: Correction Compte abstract et ajout des classes Courant/Epargne (Polymorphisme)

---
+++ SRC/model/Courant.java
@@ -0,0 +1,22 @@
+package SRC.model;
+
+public class Courant  extends Compte{
+    public Courant(String numeroCompte, double soldeInitial) {
+        super(numeroCompte, soldeInitial);
+    }
+
+    @Override 
+
+   public void depotArgent(double montant){
+      this.solde += montant;
+
+    }
+
+    @Override 
+
+  public void retraitArgent(double montant){
+            this.solde -= montant;
+    }
+    
+    
+}
