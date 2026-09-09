b58b02c849b390d888abe915c4b441db27c3c509
noureddinezn <noureddinezouana@gmail.com>
2026-09-09 17:11:16 +0100

    NEX-22: Ajout de la méthode afficherHistorique dans la classe Compte

--- SRC/model/Compte.java
+++ SRC/model/Compte.java
@@ -27,6 +27,20 @@ public Compte(String numeroCompte, double soldeInitial) {
     // Abstract methods for Polymorphism (à implémenter dans les classes filles)
     public abstract void depotArgent(double montant);
     public abstract void retraitArgent(double montant);
-
+public void afficherHistorique() {
+        System.out.println("\n--- HISTORIQUE DES TRANSACTIONS (Compte: " + this.numeroCompte + ") ---");
+        
+        if (this.historiqueTransactions.isEmpty()) {
+            System.out.println("Aucune transaction trouvee pour ce compte.");
+        } else {
+            for (Transaction tx : this.historiqueTransactions) {
+                System.out.println("- ID: " + tx.getIdTransaction() + 
+                                   " | Type: " + tx.getType() + 
+                                   " | Montant: " + tx.getMontant() + " DH" +
+                                   " | Date: " + tx.getDate());
+            }
+        }
+        System.out.println("------------------------------------------------------\n");
+    }
     
 }
