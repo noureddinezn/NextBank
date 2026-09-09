c6fed39ac352b221e9ab65a15a6f73b0409357a8
noureddinezn <noureddinezouana@gmail.com>
2026-09-09 16:07:19 +0100

    la creation des method  depotArgent et la method retraitArgent dans la class Epargne

--- SRC/model/Epargne.java
+++ SRC/model/Epargne.java
@@ -1,18 +1,30 @@
 package src.model;
 
 public class Epargne  extends Compte{
-   public Epargne(String numeroCompte, double soldeInitial) {
+  public Epargne(String numeroCompte, double soldeInitial) {
         super(numeroCompte, soldeInitial);
     }
 
-    @Override
+   @Override
     public void depotArgent(double montant) {
-        this.solde += montant;
+        if (montant > 0) {
+            this.solde += montant;
+           
+            Transaction tx = new Transaction((int) (Math.random() * 1000), TypeTransaction.Depot, montant, this.numeroCompte);
+            this.historiqueTransactions.add(tx);
+        }
     }
 
-    @Override
+  @Override
     public void retraitArgent(double montant) {
-        this.solde -= montant;
+        if (montant > 0 && this.solde >= montant) {
+            this.solde -= montant;
+           
+            Transaction tx = new Transaction((int) (Math.random() * 1000), TypeTransaction.Retrait, montant, this.numeroCompte);
+            this.historiqueTransactions.add(tx);
+        } else {
+            System.out.println("Erreur: Solde insuffisant pour le retrait du compte épargne !");
+        }
     }
     
 }
